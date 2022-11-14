7c715c61e033f00673175ced52d027c50bc9b9c3
rafaelaznar <rafaaznar@gmail.com>
2022-11-14 14:22:41 +0100

    actualize CORS

--- src/main/java/net/ausiasmarch/wildcart/filter/CORSFilter.java
+++ src/main/java/net/ausiasmarch/wildcart/filter/CORSFilter.java
@@ -33,6 +33,7 @@
 package net.ausiasmarch.wildcart.filter;
 
 import java.io.IOException;
+import java.util.Collection;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -41,11 +42,25 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CORSFilter implements Filter {
 
+      private void addSameSiteCookieAttribute(HttpServletResponse response) {
+        Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
+        boolean firstHeader = true;
+        for (String header : headers) { // there can be multiple Set-Cookie attributes
+            if (firstHeader) {
+                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "SameSite=Strict"));
+                firstHeader = false;
+                continue;
+            }
+            response.addHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "SameSite=Strict"));
+        }
+    }
+    
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
@@ -61,8 +76,14 @@ public class CORSFilter implements Filter {
         //oResponse.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Max-Age", "3600");        
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        
+          if (oRequest.getMethod().equalsIgnoreCase("OPTIONS")) {
+               response.setHeader("Access-Control-Allow-Private-Network", "true");
+          }
+         addSameSiteCookieAttribute((HttpServletResponse) response);
+        
         chain.doFilter(req, res);
             
 
